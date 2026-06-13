7e4ab7a4677bfd9ee65f023c4d9ecae6913a421e
shogodel <morteanubogdan@gmail.com>
2026-06-12 22:49:42 -0400

    chore: remove unused seo.ts helper

--- src/lib/seo.ts
+++
@@ -1,9 +0,0 @@
-export function alternatesWithDefault(path: string) {
-  return {
-    languages: {
-      "x-default": `/fr${path}`,
-      fr: `/fr${path}`,
-      en: `/en${path}`,
-    },
-  };
-}
